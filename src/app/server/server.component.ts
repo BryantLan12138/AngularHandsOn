05fa0c54244bc79e7ebdcb0498d97a07d9bc1689
BryantLan12138 <bryant.lan1212@gmail.com>
2021-07-17 22:40:08 +0930

    practice with ngclass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,16 +2,32 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+        .onshore{
+        color: white;
+       }
+    `]
 })
 
 export class ServerComponent {
-    name: string = 'Bryant';
-    activity: string = 'programming';
+    name: string = 'CNA';
+    status: string = 'offshore';
+
+    constructor()
+    {
+        this.status = Math.random() > 0.5 ? 'onshore' : 'offshore' ;
+        this.name = Math.random() > 0.5 ? 'CNA' : 'ADSA';
+    }
 
     getName()
     {
         return this.name;
+    }
+
+    getColor()
+    {
+        return this.status === 'onshore' ? 'green' : 'red';
     }
 
 }
